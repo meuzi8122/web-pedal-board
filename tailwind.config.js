c4b67455797b99dfe967e592ad812ee9e4e94a49
meuzi8122 <kagiakete1234@gmail.com>
2024-10-12 23:35:34 +0900

    tailwind.configのcontent追記

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import("tailwindcss").Config} */
 export default {
+  content: ["./components/**/*.vue", "./layouts/**/*.vue", "./pages/**/*.vue", "./app.vue"],
   daisyui: {
     themes: ["aqua"],
   },
