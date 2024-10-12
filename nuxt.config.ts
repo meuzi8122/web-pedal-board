bf4b922cd21f3ccb8aa6e1dcc0cc26a2aa6b83e8
meuzi8122 <kagiakete1234@gmail.com>
2024-10-12 20:55:25 +0900

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Web PedalBoard",
+    },
+  },
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+});
